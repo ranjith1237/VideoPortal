4862245f1742701efd08456c71eed7e5cb6af47b
ranjith1237 <ranjithreddy1061995@gmail.com>
2019-06-01 21:35:01 +0530

    UI for allvideos and contract changes with Violations Portal

--- Cyberabad/views.py
+++ Cyberabad/views.py
@@ -40,7 +40,7 @@ def upload_Video(request):
         thumbnail_path = os.path.join(tempPath,str(video_Saved.pk))
         os.mkdir(thumbnail_path)
         videoPath = os.path.join(settings.BASE_DIR,'media',str(video_Saved.videofile))
-        chunk_Video_data.delay(videoPath,thumbnail_path)
+        chunk_Video_data.delay(videoPath,thumbnail_path,video_Saved.pk)
         getLocations.delay(gps_filePath,video_Saved.pk)
     context = {
         'form':form
@@ -50,12 +50,14 @@ def upload_Video(request):
 @login_required()
 def all_Videos(request):
     allVideos = Video.objects.all()
-    paginator = Paginator(allVideos, 1) 
+    paginator = Paginator(allVideos, 8) 
 
     page = request.GET.get('page')
     page_videos = paginator.get_page(page)
+    pageVideos = [page_videos[x:x+4] for x in range(0, len(page_videos), 4)]
     context = {
         'allVideos':page_videos,
+        'pageVideos':pageVideos
     }
     return render(request,'videos.html',context)
 
@@ -103,10 +105,16 @@ def display_Video(request, id):
 
 def rithish(request):
     payload={
-        'chunks':31,
-        'id':1
+        'id':8
     }
-    response=requests.post('http://10.2.40.44:9000/newVideo/',payload)
+    video_path = os.path.join(settings.BASE_DIR,'media','data',str(payload['id']))
+    chunks=0
+    for li in os.listdir(video_path):
+        if li.endswith(".ts"):
+            chunks+=1
+    payload['chunks']=chunks
+    print(payload)
+    response=requests.post('http://10.4.16.53:9000/newVideo/',payload)
     if response.status_code==200:
         print("working")
         print(response.json())
@@ -134,7 +142,7 @@ def sendMedia(request):
             return response
         chunk = request.POST.get('chunk',0)
         file_name='output'+str(chunk)+'.ts'
-        folder_path = os.path.join(settings.BASE_DIR,'media','data','downloads','ts_files')
+        folder_path = os.path.join(settings.BASE_DIR,'media','data',str(id))
         file_path = os.path.join(folder_path,file_name)
         file_wrapper = FileWrapper(open(file_path, 'rb'))
         file_mimetype = 'video/ts'

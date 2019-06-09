0027441a71ab812d1ffc2d1437d0f5e9c1c60583
ranjith1237 <ranjithreddy1061995@gmail.com>
2019-06-09 09:35:29 +0530

    added scheduler functioality

--- manage.py
+++ manage.py
@@ -2,7 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import schedule
+import subprocess
+import time
+from threading import Thread
 
+def job():
+    subprocess.call(cmd,shell=True)
+
+def runscheduler():
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
@@ -18,4 +30,7 @@ def main():
 
 
 if __name__ == '__main__':
+    schedule.every().day.at("23:51").do(job)
+    p=Thread(target=runscheduler)
+    p.start()
     main()
